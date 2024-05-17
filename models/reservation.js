c30a7bf43515ba36c70db2e2e9295fe93b022724
Anya Aven <leafeon96@gmail.com>
2024-05-17 15:11:09 -0400

    Added save() functionality and fixed return bug in getFormat

--- models/reservation.js
+++ models/reservation.js
@@ -17,7 +17,7 @@ class Reservation {
   /** formatter for startAt */
 
   getFormattedStartAt() {
-    datefns.format(this.startAt, "MMMM d yyyy, h:mm a")
+    return datefns.format(this.startAt, "MMMM d yyyy, h:mm a")
   }
 
   /** given a customer id, find their reservations. */
@@ -36,6 +36,41 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
+
+
+  /** Save this reservation to the DB.
+   * This adds a reservation if they are not located already in the DB.
+   * Otherwise we update the reservation's information for:
+   * numGuests, startAt, and notes.
+  */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests, this.notes],
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations
+             SET customer_id=$1,
+                 start_at=$2,
+                 num_guests=$3,
+                 notes=$4
+             WHERE id = $5`, [
+        this.customerId,
+        this.startAt,
+        this.numGuests,
+        this.notes,
+        this.id,
+      ],
+      );
+    }
+  }
+
 }
 
 

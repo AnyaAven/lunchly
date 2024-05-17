f394e9bd0ddd07ffb4b0d71b2208ddf25427b765
Anya Aven <leafeon96@gmail.com>
2024-05-17 18:36:21 -0400

    Removed middleware

--- middleware.js
+++ middleware.js
@@ -8,20 +8,5 @@ function logger(req, res, next) {
   return next();
 }
 
-/** Checks if the route query contains "/?search="
-    * If so, passing a filtered list of customers by search term value.
-    * Else, passing list of all customers.
- * List of customers will be stored within res.locals.customers */
 
-async function checkSearchQuery(req, res, next) {
-  if ("search" in req.query) {
-    const searchValue = req.query.search;
-    res.locals.customers = await Customer.getAllCustomersBySearch(searchValue);
-  } else {
-    res.locals.customers = await Customer.all();
-  }
-  next();
-}
-
-
-export { logger, checkSearchQuery };
+export { logger };
